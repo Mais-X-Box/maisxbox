80022d0e3480adb0d41418e08a05fc17c9e55b73
Raphael Augusto da Silva <raphaaugustosilva@gmail.com>
2021-09-02 21:58:20 -0300

    Retirando duration

--- lib/presentation/presenters/home/home_presenter.dart
+++ lib/presentation/presenters/home/home_presenter.dart
@@ -35,7 +35,6 @@ abstract class _HomePresenterBase extends MobxBaseStore with Store {
   Future<void> loadPartners() async {
     try {
       this.homeViewModel.setIsPartnersLoading(true);
-      await Future.delayed(Duration(seconds: 2));
       var partners = await loadAllPartners.execute();
       this.homeViewModel.setPartnerList(partners ?? []);
     } catch (e) {
