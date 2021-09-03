a16edfaf905693826b63a7463c828a14bf15fe08
Raphael Augusto da Silva <raphaaugustosilva@gmail.com>
2021-09-02 22:13:34 -0300

    Ajustes

--- lib/presentation/ui/pages/home/list_blocks/home_page_list_block_base.dart
+++ lib/presentation/ui/pages/home/list_blocks/home_page_list_block_base.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:maisxbox/domain/entities/game/game_entity.dart';
 import 'package:maisxbox/presentation/presenters/home/home_presenter.dart';
 import 'package:maisxbox/presentation/ui/components/loading_component.dart';
